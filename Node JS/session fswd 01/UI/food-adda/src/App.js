5f51fa905603438698930373e6ebc289292b1450
Sandeep G <dev.sandeep@hotmail.com>
2023-03-11 08:15:09 +0530

    changes in reference page

--- Node JS/session fswd 01/UI/food-adda/src/App.js
+++ Node JS/session fswd 01/UI/food-adda/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import { NavbarTop } from "./components/NavbarTop";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { ProductDetail } from "./pages/ProductDetail";
+import { Home } from "./Pages/Home";
+import { Login } from "./Pages/Login";
+import { ProductDetail } from "./Pages/ProductDetail";
 import { Footer } from "./components/Footer";
 import { ToastContainer } from "react-toastify";
-import Cart from "./pages/Cart";
-import Address from "./pages/Address";
-import Checkout from "./pages/Checkout";
-import Order from "./pages/Order";
+import Cart from "./Pages/Cart";
+import Address from "./Pages/Address";
+import Checkout from "./Pages/Checkout";
+import Order from "./Pages/Order";
 
 function App() {
   return (
