331c4979f9a847c0e421e7c7066dfa6471693ca9
17862536 <116952162+17862536@users.noreply.github.com>
2023-03-16 02:00:34 +0530

    edit address completed

--- Node JS/session fswd 01/Node/src/app/routes.js
+++ Node JS/session fswd 01/Node/src/app/routes.js
@@ -17,10 +17,17 @@ exports.loadRoutes = (app) => {
   // user related goes here
   app.post("/signup", signupMiddleware, signup);
   app.post("/login", login);
+<<<<<<< Updated upstream
   app.get("/item/:item_id", productDetails); // blame shreyas if any issues
   app.post("/order", checkout); // blame shreyas if any issues
   app.get("/address", getAddress); // blame shreyas and sakshi if any issues
   app.post("/create_address", addAddress); // blame prem if any issues
+=======
+  app.get("/item/:item_id", productDetails);
+  app.post("/order", checkout);
+  app.get("/address", getAddress);
+  app.post("/create_address", addAddress)
+>>>>>>> Stashed changes
 };
 //routes related to home page - home page
 //routes related to user activity - login, signup, checkout, order, address
--- Node JS/session fswd 01/Node/src/app/user/editAddress.js
+++ Node JS/session fswd 01/Node/src/app/user/editAddress.js
@@ -1,56 +1,39 @@
-const { Address } = require("../../model/Address");
+const { fetchCustomerID } = require("../../utils/getID");
+const { Address } = require("../../../model/Address");
+const { responseCreator } = require("../../utils/responseCreator");
 
-exports.editAddress = async (req, res, next) => {
-  const isEdit = await Address.edit({ line1: req.body.line1 });
-  if (!isEdit) {
-    next();
+exports.editAddress = async (request, response) => {
+  if (request.header("user_id") !== undefined) {
+    const customerData = await fetchCustomerID(request.header("user_id"));
+    const customerID = customerData !== undefined ? customerData.id : undefined;
+    const { addressid, line1, line2, city, state, phone } = request.body;
+    if (addressid !== undefined) {
+      if (
+        line1 !== undefined &&
+        line2 !== undefined &&
+        city !== undefined &&
+        state !== undefined &&
+        phone !== undefined
+      ) {
+        await Address.findOneAndUpdate(
+          { id: addressid },
+          {
+            id: addressid,
+            line1: line1,
+            line2: line2,
+            city: city,
+            state: state,
+            phone: phone,
+            cid: customerID,
+          }
+        );
+      } else {
+        response.status(400).send(responseCreator("details not found"));
+      }
+    } else {
+      response.status(400).send(responseCreator("address not found"));
+    }
   } else {
-    const resp = Address("line1 already exists");
-    res.status(500).send(resp);
-  }
-};
-exports.editAddress = async (req, res, next) => {
-  const isEdit = await Address.edit({ line2: req.body.line2 });
-  if (!isEdit) {
-    next();
-  } else {
-    const resp = Address("line2 already exists");
-    res.status(500).send(resp);
-  }
-};
-exports.editAddress = async (req, res, next) => {
-  const isEdit = await Address.edit({ city: req.body.city });
-  if (!isEdit) {
-    next();
-  } else {
-    const resp = Address("city already exists");
-    res.status(500).send(resp);
-  }
-};
-exports.editAddress = async (req, res, next) => {
-  const isEdit = await Address.edit({ state: req.body.state });
-  if (!isEdit) {
-    next();
-  } else {
-    const resp = Address("state already exists");
-    res.status(500).send(resp);
-  }
-};
-exports.editAddress = async (req, res, next) => {
-  const isEdit = await Address.edit({ phone: req.body.phone });
-  if (!isEdit) {
-    next();
-  } else {
-    const resp = Address("phoneno already exists");
-    res.status(500).send(resp);
-  }
-};
-exports.editAddress = async (req, res, next) => {
-  const isEdit = await Address.edit({ cid: req.body.cid });
-  if (!isEdit) {
-    next();
-  } else {
-    const resp = Address("cid already exists");
-    res.status(500).send(resp);
+    response.status(400).send(responseCreator("token not found"));
   }
 };
